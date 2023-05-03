de28e5b54d61b06e3e4e84a03748fcf7f81cdfde
ConorC90 <conorcarey90@gmail.com>
2023-05-03 15:44:05 +0200

    spellig corections

--- src/content/RegisterBlockContent.tsx
+++ src/content/RegisterBlockContent.tsx
@@ -4,15 +4,15 @@ export default function RegisterBlockContnet() {
   return (
     <>
       <p>
-        Experience the ultimate beach vacation at the Porró Open tournament.
-        Join us for three days of mixed ultimate frisbee on the beautiful
-        beaches of Castelldefels near Barcelona.
+        Sign up now and experience all that the Porró Open has to offer. Join us
+        for three days of mixed ultimate on the beautiful beaches of
+        Castelldefels near Barcelona.
       </p>
       <p>
-        Along with playing ultimate frisbee, you'll have the opportunity to meet
-        new people, enjoy free beer, and a Sunday night fancy dress party. Don't
-        miss out on this unique and unforgettable beach vacation experience at
-        the Porró Open tournament!
+        Along with 3 days of ultimate, you'll have the opportunity to, party on
+        the beach, meet new people, enjoy free beer, and a Sunday night 90s
+        theamed fancy dress party. Don't miss out on this unique and
+        unforgettable beach ultimate experience!
       </p>
     </>
   );
