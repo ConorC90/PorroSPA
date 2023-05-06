e890e6f2ae968ef43d8ecfc47af3c9f1d6c0e441
ConorC90 <conorcarey90@gmail.com>
2023-05-06 18:48:50 +0200

    copy write check

--- src/content/AboutBlockContent.tsx
+++ src/content/AboutBlockContent.tsx
@@ -5,13 +5,13 @@ export default function AboutContent() {
     <>
       <p>
         Don't miss out on the 25th edition of the Porró Open. Barcelona's
-        premier ultimate frisbee tournament, held on the beautiful beaches of
-        Castelldefels. The sea, sand, and sun are waiting for you.
+        premier Ultimate Frisbee tournament, held on the beautiful beaches of
+        Castelldefels. The sea, sand and sun are waiting for you.
       </p>
       <p>
         For the 25th edition of the Porró we are going back to the year of the
         Porró inception, 1998. Join players from around the world for three days
-        of beach ultimate and parties, as we celebrate the 90s in classic Porró
+        of Beach Ultimate and parties, as we celebrate the 90s in classic Porró
         style.
       </p>
       <p>
